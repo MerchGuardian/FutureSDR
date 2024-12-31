eb233c9e731a5fc982e5c175ef1d67f2a9047c46
Troy Neubauer <troyneubauer@gmail.com>
2024-12-31 10:49:41 -0600

    cleanup diff

--- src/runtime/logging_android.rs
+++ src/runtime/logging_android.rs
@@ -3,5 +3,6 @@
 // use android_logger::Config;
 
 pub fn init() {
+    // We use our own logger, so dont initialize this one
     // android_logger::init_once(Config::default().with_max_level(LevelFilter::Debug));
 }
